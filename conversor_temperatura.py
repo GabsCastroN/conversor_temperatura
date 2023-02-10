d8119fc22a689f456806748686ab90e72a7876be
GabsCastroN <gabrielcastros@colegioguanenta.edu.co>
2023-02-10 09:07:50 -0500

    Se creo el codigo PY y su respectiva funcion

---
+++ conversor_temperatura.py
@@ -0,0 +1,15 @@
+#Conversor de temperatura
+
+#Input
+
+c = int (input ("Digite °C: "))
+
+#processing 
+
+f = c * 9/5 + 32 
+k = c + 273.15
+
+#output
+
+print ("Farenheit " + str(f))
+print ("Kelvin " + str(k))
